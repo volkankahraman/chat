51dd4ba30d37f83a1efa25650a14ce0bb6ba1e15
Volkan Kahraman <khrmnvolkan@gmail.com>
2022-04-25 03:16:44 +0300

    deploy

---
+++ precache-manifest.1314804ff5c26fd1facdd39c151bfaef.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe4e3be1a250e99290e",
+    "url": "/chat/css/app.5ac75c14.css"
+  },
+  {
+    "revision": "46a9f9a70d7c62562d49",
+    "url": "/chat/css/chunk-vendors.2715b064.css"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/chat/favicon.ico"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/chat/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/chat/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/chat/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/chat/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/chat/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/chat/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/chat/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/chat/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/chat/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/chat/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/chat/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/chat/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/chat/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/chat/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "6de16254d1b4193353e7015f9177535d",
+    "url": "/chat/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/chat/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "429ac18b6cedbd702b6ecd1d2bb52fa8",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "1fe4e3be1a250e99290e",
+    "url": "/chat/js/app.cd2f371d.js"
+  },
+  {
+    "revision": "46a9f9a70d7c62562d49",
+    "url": "/chat/js/chunk-vendors.976e4ca2.js"
+  },
+  {
+    "revision": "5ca358a01353c51ec454805d363fe482",
+    "url": "/chat/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/chat/robots.txt"
+  }
+]);
